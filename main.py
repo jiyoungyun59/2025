632c0f110bec3997fdfded09f9b7a474ebc9233b
jiyoungyun59 <0822jyy@naver.com>
2025-08-18 12:02:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+# 제목
+st.set_page_config(page_title="✨ MBTI 진로 추천 웹앱 ✨", page_icon="🌈")
+st.title("🌟 MBTI 기반 진로 추천 서비스 🌟")
+
+st.write("""
+안녕하세요! 🎉 이곳은 **MBTI를 기반으로 적합한 직업을 추천**해주는 진로 교육용 웹앱이에요 💼💡
+당신의 성격유형을 선택하고 미래에 어울릴 직업을 확인해보세요 🚀
+""")
 
